2e961e9579a4ee110730a764ab301bcd195428a0
vinusham10 <vinusham05@gmail.com>
2024-02-22 09:56:35 +0530

    created a branch locally, created react app, and created a landing page

---
+++ app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>ToonTopia</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
