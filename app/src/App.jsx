f019c264bfc49ae58328fc8703cb9e44b163d546
vinusham10 <vinusham05@gmail.com>
2024-02-22 15:32:43 +0530

    created landing page using react

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,31 @@
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react';
+import './App.css';
+import bg from './assets/finalig1.jpg'
+const App = () => {
   return (
     <>
-      <div>
-        <h1>ToonTopia</h1>
+     <div className="right-image">
+    <img src={bg} alt="Right Image" />
+  </div>
+    <div className="toontopia-container">
+      <div className="content">
+        <header>
+          <h1>ToonTopia</h1>
+        </header>
+        <main>
+          <h2>Welcome to the Cartoon Paradise!</h2>
+          <p>Discover a world of animated adventures at ToonTopia.</p>
+          <div className="auth-buttons">
+            <button className="login-button">Login</button>
+            <button className="signup-button">Sign Up</button>
+          </div>
+        </main>
       </div>
-    </>
-  )
-}
+      
+    </div>
+   
+  </>
+  );
+};
 
-export default App
+export default App;
