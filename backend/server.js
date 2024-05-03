0521074a5564d99cff3affd7db3532fca6faa98e
vinusham10 <vinusham05@gmail.com>
2024-05-03 11:55:58 +0530

    removed node_modules

--- backend/server.js
+++ backend/server.js
@@ -12,19 +12,16 @@ const mongoURI = process.env.MONGODB_URI;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-// Check connection status
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
     console.log('Connected to MongoDB');
 });
 
-// Define the home route
 app.get('/', (req, res) => {
     res.send(`Welcome to the home page. Database connection status: ${db.readyState === 1 ? 'Connected' : 'Not Connected'}`);
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
