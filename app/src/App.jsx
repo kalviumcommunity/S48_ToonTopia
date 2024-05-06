c32078da3e347d5c384fffec5f45a1049bb1e8e2
vinusham10 <vinusham05@gmail.com>
2024-05-06 22:51:46 +0530

    designing a component

---
+++ app/src/App.jsx
@@ -0,0 +1,19 @@
+
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+
+const App = () => {
+  return (
+<Router>
+<Routes>
+  <Route path="/" element={<HomePage />} />
+  <Route path="/signup" element={<SignUp />} />
+  <Route path="/login" element={<Login />} />
+</Routes>
+</Router>
+);
+};
